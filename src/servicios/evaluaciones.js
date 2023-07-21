3bca6bdb7de6a66f53b467e058f0e96258e182a4
Andres Vera <andyvercha@gmail.com>
2023-07-21 18:14:54 -0400

    upd 21-7-23

--- src/servicios/evaluaciones.js
+++ src/servicios/evaluaciones.js
@@ -70,20 +70,22 @@ routes.get('/getproceso/:idconvocatoria/:idmateria', verificaToken, async (req, 
 routes.get('/getprocesoinscripcion/:idconvocatoria/:idinscripcion', verificaToken, async (req, res) => {
     try {
         await database.query(`CALL p_genera_proceso(${req.params.idconvocatoria},@a);`);
-        const rs_evaluaciones = await database.query(`select * from vw_proceso_materia where idconvocatoria= ${req.params.idconvocatoria} and idinscripcion=${req.params.idinscripcion}`, { type: QueryTypes.SELECT });
-        jwt.verify(req.token, process.env.CLAVESECRETA, (err, authData) => {
-            if (err) {
-                res.json({ error: "Error de autenticacion, vuelva a iniciar la sesion, sino, contacte con el administrador", err });
-            } else {
-                res.json({
-                    mensaje: "successfully",
-                    authData: authData,
-                    body: rs_evaluaciones
-                })
-            }
+        await database.query(`select * from vw_proceso_materia where idconvocatoria= ${req.params.idconvocatoria} and idinscripcion=${req.params.idinscripcion}`, { type: QueryTypes.SELECT }).then((rs_evaluaciones) => {
+            jwt.verify(req.token, process.env.CLAVESECRETA, (err, authData) => {
+                if (err) {
+                    res.json({ error: "Error de autenticacion, vuelva a iniciar la sesion, sino, contacte con el administrador", err });
+                } else {
+                    res.json({
+                        mensaje: "successfully",
+                        authData: authData,
+                        body: rs_evaluaciones
+                    })
+                }
+            })
         })
+
     } catch (error) {
-        res.json({ error: "Error en el servidor, verifique los campos cargados, sino contacte con el administrador" });
+        res.json({ error: "Error en el servidor, verifique los campos cargados, sino contacte con el administrador, ",error });
     }
 })
 
